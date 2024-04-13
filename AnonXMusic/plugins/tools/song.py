20fed37a7c69975d6cb58fef2e006c935c543a74
NoName <57182847+ali-now447@users.noreply.github.com>
2024-04-13 19:27:25 +0300

    Update song.py

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -1,1 +1,86 @@
+import os
 
+import requests
+import yt_dlp
+from pyrogram import filters
+from strings.filters import command
+from youtube_search import YoutubeSearch
+
+from AnonXMusic import app
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+@app.on_message(command(["/song", "بحث","تحميل","تنزيل","يوت"]))
+def song(client, message):
+
+    message.delete()
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    chutiya = message.from_user.mention
+
+    query = ""
+    for i in message.command[1:]:
+        query += " " + str(i)
+    print(query)
+    m = message.reply("جاࢪ البحث لحظة...")
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        # print(results)
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+
+        duration = results[0]["duration"]
+        results[0]["url_suffix"]
+        views = results[0]["views"]
+
+    except Exception as e:
+        m.edit(
+            "لم يتم العثوࢪ على الأغنية حاول مرة أخࢪى!"
+        )
+        print(str(e))
+        return
+    m.edit("جاࢪِ التنزيل...أنتظر لحظة!")
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f"الأسم: {title[:25]}\nالمدة: {duration}\nالمشاهدات: {views}\nبواسطة:​ {chutiya}"
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
+        message.reply_audio(
+            audio_file,
+            caption=rep,
+            performer="@zzsvv . برود",
+            thumb=thumb_name,
+            title=title,
+            duration=dur,
+        )
+        m.delete()
+    except Exception as e:
+        m.edit(
+            f"[Victorious](t.me/zzsvv) 💕**\n\**خطأ :** {e}"
+        )
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
+
+__mod_name__ = "اليوتيوب"
+__help__ = """
+بحث او تحميل مع رابط الاغنية او اسمها """
