c58c34aafb5f3f8e5c1de757cc14136779aeeb2d
NoName <57182847+ali-now447@users.noreply.github.com>
2024-04-13 19:13:43 +0300

    Update filters.py

--- strings/filters.py
+++ strings/filters.py
@@ -1,1 +1,9 @@
+from typing import List, Union
 
+from pyrogram import filters
+
+
+other_filters = filters.group
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, "")
