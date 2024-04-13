8fe1f46a80af1278d63a41a0be95a9b678140e4f
NoName <57182847+ali-now447@users.noreply.github.com>
2024-04-13 19:29:46 +0300

    Create must_join.py

---
+++ AnonXMusic/plugins/tools/must_join.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from AnonXMusic import app
+
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not "https://t.me/zzsvv":  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member("zzsvv", msg.from_user.id)
+        except UserNotParticipant:
+            if "https://t.me/zzsvv".isalpha():
+                link = "https://t.me/zzsvv"
+            else:
+                chat_info = await bot.get_chat("zzsvv")
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @zzsvv .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("اضغط للأشتراك .", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I'm not admin in the MUST_JOIN chat @zzsvv !")
