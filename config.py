7f84503773d7ded26b0b7318408bb00ba2ccf710
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-04-30 05:43:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ali-now447/xox",
+    "https://github.com/Hjwad/xox",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)  
